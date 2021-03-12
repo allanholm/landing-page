f38f7b581a41f7bb3cd38c6976fe6f08a2afd9e2
Allan Holm Søndergaard <6605392@rts-365.dk>
2021-03-12 09:26:45 +0100

    add analytics

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -3,7 +3,7 @@ import "./GalleryItem.scss";
 export default function GalleryItem({image, copy, ctaLink, ctaCopy}) {
   return(
         <div className="carouselItem">
-          <img src={image} alt=""className="carouselItem__image"/>
+          <img src={image} alt="of something"className="carouselItem__image"/>
           <div className="carouselItem__content">
             <p className="carouselItem__copy">{copy}</p>
             <a href={ctaLink} className="carouselItem__CTA">{ctaCopy}</a>
